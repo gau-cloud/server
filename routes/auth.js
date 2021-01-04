d93ccfb6122f9ed68ad0695000abeaf226d545d6
camel-man-ims <gudwnsrh@gmail.com>
2021-01-05 02:49:46 +0900

    21-01-05

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ var router = express.Router();
 const upload = require('../modules/multer')
 const userController = require('../controllers/user')
 const authUtil = require('../middlewares/auth').checkToken
+const jwtAuth = require('../middlewares/auth').jwtauth
+
 
 // 1. 회원가입
 router.post('/signup',upload.single('data'),userController.signup)
@@ -19,7 +21,7 @@ router.get('/post-comments-likes',authUtil,userController.getPostCommentsLikes)
 // 5. 내가 올린 게시글 불러오기
 router.get('/post-mine',authUtil,userController.getPostOfUser)
 
-router.post('/jwtauth', authUtil, userController.getJwtAuth)
+router.get('/jwtauth', jwtAuth, userController.getJwtAuth)
 
 router.get('/test',authUtil,userController.test)
 
